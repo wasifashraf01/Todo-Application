3cd90bbd77e70d365237bc74defcd97d0ab2f216
wasifashraf01 <mw431019@gmail.com>
2025-11-05 04:41:00 +0500

    Add Firebase integration for todo application
    Integrate Firebase for todo management, including add, delete, and edit functionalities.

--- app.js
+++ app.js
@@ -1,58 +1,98 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD8YoCf1Fakme9odzNvrZt-vgp3Zh_5AUM",
+  authDomain: "todo-application-a001.firebaseapp.com",
+  databaseURL: "https://todo-application-a001-default-rtdb.firebaseio.com",
+  projectId: "todo-application-a001",
+  storageBucket: "todo-application-a001.firebasestorage.app",
+  messagingSenderId: "176767202366",
+  appId: "1:176767202366:web:996dab3aa5a1389ab30d6f"
+};
+
+const frb = firebase.initializeApp(firebaseConfig);
+console.log(frb.database);
+
 var input = document.getElementById("todo");
 
 function addTodo() {
   if (input.value.trim() !== "") {
+    var key = firebase.database().ref("todos").push().key;
+
+    var obj = {
+      UserValue: input.value,
+      Key: key,
+    };
+
+    firebase.database().ref("todos").child(key).set(obj);
+
     var liELement = document.createElement("li");
     var liText = document.createTextNode(input.value);
-    var ulELement = document.getElementById("todoList");
-
-    // ###############delete button element#################
-
-    var delBtnELEMENT = document.createElement("button");
-    var delBtnText = document.createTextNode("Delete");
-
-    delBtnELEMENT.appendChild(delBtnText);
-
-    delBtnELEMENT.setAttribute("onclick", "deleteSingleItem(this)");
-
-    // ###############edit button element#################
-
-    var editBtnELEMENT = document.createElement("button");
-
-    var editBtnText = document.createTextNode("Edit");
-
-    editBtnELEMENT.appendChild(editBtnText);
-
-    editBtnELEMENT.setAttribute("onclick", "editSingleItem(this)");
-
     liELement.appendChild(liText);
+    liELement.setAttribute("data-key", key);
+    liELement.style.fontSize = "2rem";
 
-    liELement.style.fontSize = "2.5rem";
+    var delBtn = document.createElement("button");
+    delBtn.textContent = "Delete";
+    delBtn.setAttribute("onclick", "deleteSingleItem(this)");
+    delBtn.setAttribute("data-key", key);
 
-    liELement.appendChild(delBtnELEMENT);
+    var editBtn = document.createElement("button");
+    editBtn.textContent = "Edit";
+    editBtn.setAttribute("onclick", "editSingleItem(this)");
 
-    liELement.appendChild(editBtnELEMENT);
-
-    ulELement.appendChild(liELement);
+    liELement.appendChild(delBtn);
+    liELement.appendChild(editBtn);
+    document.getElementById("todoList").appendChild(liELement);
 
     input.value = "";
   } else {
-    alert("required fields are missings");
+    alert("Please enter a task!");
   }
 }
 
+// Delete All
 function deleteAll() {
-  var ulElement = document.getElementById("todoList");
-
-  ulElement.innerHTML = "";
+  var confirmDelete = confirm("Are you sure you want to delete all todos?");
+  if (confirmDelete) {
+    // Firebase se delete
+    firebase.database().ref("todos").remove()
+    .then(() => {
+      console.log("All todos deleted from Firebase");
+      // UI se bhi delete
+      document.getElementById("todoList").innerHTML = "";
+    })
+    .catch((error) => {
+      console.error("Error deleting all todos:", error);
+    });
+  }
 }
 
+
+// Delete 1 Item
 function deleteSingleItem(e) {
-  e.parentNode.remove();
+  var li = e.parentNode;
+  var key = e.getAttribute("data-key"); 
+  firebase.database().ref("todos").child(key).remove(); 
+  li.remove(); 
 }
 
-function editSingleItem(e) {
-  var updatedValue = prompt("Enter  your task");
 
-  e.parentNode.firstChild.nodeValue = updatedValue;
+// Edit Item
+function editSingleItem(e) {
+  var li = e.parentNode;
+  var key = li.getAttribute("data-key");
+  var updatedValue = prompt("Enter your updated task", li.firstChild.nodeValue);
+
+  if (updatedValue !== null && updatedValue.trim() !== "") {
+    // Firebase update
+    firebase.database().ref("todos").child(key).update({
+      UserValue: updatedValue
+    })
+    .then(() => {
+      console.log("Firebase Updated");
+      li.firstChild.nodeValue = updatedValue; // UI update
+    })
+    .catch((error) => {
+      console.error("Error updating Firebase:", error);
+    });
+  }
 }
